71dd59067d6212de902bd769086b0608c655b81f
Vincent Liao <vincent.liao@protonmail.com>
2022-01-26 19:19:11 +0700

    finish event.rs (still logical error)

--- src/event.rs
+++ src/event.rs
@@ -6,19 +6,19 @@ fun::{marker::*, Scalar, nonce},
 use hex;
 use sha2::{Sha256, Digest};
 use rand::rngs::ThreadRng;
-use json::array;
+use json::{array, object};
 
-struct Event {
-    id: String,
-    pubkey: String,
-    created_at: u32,
-    kind: u32,
-    tags: [String; 0],
-    content: String,
-    sig: String
-}
+// struct Event {
+//     id: String,
+//     pubkey: String,
+//     created_at: u32,
+//     kind: u32,
+//     tags: [String; 0],
+//     content: String,
+//     sig: String
+// }
 
-pub fn create_event() {
+pub fn create_event() -> String {
     // steps:
     // 1. generate key
     // 2. hash data (as aligned in nip-01)
@@ -38,29 +38,33 @@ pub fn create_event() {
     let keypair = schnorr.new_keypair(Scalar::random(&mut rand::thread_rng()));
     let (secret_key, public_key) = keypair.as_tuple();
 
-    println!("{}", secret_key);
-    println!("{}", public_key);
-
     // create data
     // [0, toHexString(publicKey), unixTime, 1, [], content];
     let data = array![0, public_key.to_string(), unix_time, 1, [], content.to_string()];
+    let data_string = data.dump();
 
     // hash data
     let mut hasher = Sha256::new();
     // dump turns it to string, as bytes turns it to byte array
-    hasher.update(data.dump().as_bytes());
+    hasher.update(data_string.as_bytes());
     let event_id = hasher.finalize();    
     let event_id_hex = hex::encode(event_id);
-    println!("{}", event_id_hex);
-    println!("{}", data.dump());
 
-    // // Sign a variable length message
-    // let message = Message::<Public>::plain("the-times-of-london", b"Chancellor on brink of second bailout for banks");
-    // // Sign the message with our keypair
-    // let signature = schnorr.sign(&keypair, message);
-    // // Get the verifier's key
-    // let verification_key = keypair.verification_key();
-    // // Check it's valid 🍿
-    // assert!(schnorr.verify(&verification_key, message, &signature));
+    let message = Message::<Public>::plain("asdf", data_string.as_bytes());
+    let signature = schnorr.sign(&keypair, message); // this signature isn't hex
+    let signature_bytes = signature.to_bytes();
+    let signature_hex = hex::encode(signature_bytes);
 
+    let event = object!{
+        id: event_id_hex,
+        pubkey: public_key.to_string(),
+        created_at: unix_time,
+        kind: 1,
+        tags: [],
+        content: content.to_string(),
+        sig: signature_hex
+    };
+    
+    println!("{}", event.dump());
+    return event.dump();
 }
