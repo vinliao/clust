4ff3dfe89291755d75366c90e682ff05f74622ef
Vincent Liao <vincent.liao@protonmail.com>
2022-01-27 14:40:13 +0700

    delete private.rs and signer.rs

--- src/private.rs
+++
@@ -1,4 +0,0 @@
-// set and generate keys
-// keys are stored in a dotfile (somewhere)
-// it can either be generated or inputted
-// keys are then used to sign in signer.rs
--- src/signer.rs
+++
@@ -1,3 +0,0 @@
-// todo: move "sign" part in event.rs here
-// purpose: input event and return signature
-// get key from private.rs
