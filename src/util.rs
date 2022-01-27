7281559eb2141ade3124d69cd1b7c7f44aa31b23
Vincent Liao <vincent.liao@protonmail.com>
2022-01-27 17:06:54 +0700

    read privkey from file

---
+++ src/util.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use serde_json;
+
+pub fn get_privkey() -> String {
+    let data = fs::read_to_string("clust.json").expect("Unable to read file");
+    let json_data: serde_json::Value = serde_json::from_str(&data).expect("Fail to parse");
+
+    return json_data["privkey"].to_string();
+}
+
+pub fn set_privkey() {
+    // set privkey
+}
+
+pub fn add_relay() {
+    // add a relay to .clustrc
+}
+
+pub fn remove_relay() {
+    // remove a relay to .clustrc
+}
