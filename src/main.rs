9a994053f8da9bfd0c563d2f41ed6fbea7968c18
Vincent Liao <vincent.liao@protonmail.com>
2022-02-09 21:37:18 +0700

    delete unused functions, implementing apis from readme

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ struct Cli {
     // maybe there's a better way to do this
     #[clap(default_value = "")]
     subcommand: String,
+
+    #[clap(default_value = "")]
+    message: String,
 }
 
 fn main() {
@@ -29,19 +32,8 @@ fn main() {
         publisher::publish_raw(args.subcommand);
     } else if args.command == "get-event" {
         getter::get_event(args.subcommand);
-    } else if args.command == "message-send" {
-        let dummy_privkey = "d6ba84470e3da5c945251799062bbe2b52774321b9a43c90d548dd283728d7d9";
-        let dummy_pubkey = "7b27c478232bbc9791d403b1db67e9696d87e7e6fff4e57a9cdfdedb754ad475";
-        let message = util::create_message(args.subcommand, dummy_pubkey.to_string());
-        // publisher::publish(message);
-    } else if args.command == "broadcast" {
-        // broadcast is really a bad name
-        // ideally not exposed to end user
-        let dummy_pubkey = "7b27c478232bbc9791d403b1db67e9696d87e7e6fff4e57a9cdfdedb754ad475";
-        util::send_encrypted_pubkey(dummy_pubkey.to_string())
-    } else if args.command == "pull" {
-        // broadcast is really a bad name
-        // ideally not exposed to end user
-        util::pull_and_decrypt();
+    } else if args.command == "create-dm-throwaway-key" {
+        util::create_dm_throwaway_key(args.subcommand, args.message);
+    }else if args.command == "create-alias" {
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -23,13 +23,12 @@ pub fn generate_key() -> (secp256k1::SecretKey, secp256k1::XOnlyPublicKey) {
     return (privkey, pubkey);
 }
 
-pub fn send_encrypted_pubkey(recipient_pub_hex: String) {
+pub fn create_dm_throwaway_key(recipient_pub_hex: String, message: String) -> serde_json::Value {
     // encrypt sender's pubkey to recipient so both can have shared key
     let secp = Secp256k1::new();
 
     let (throwaway_privkey, throwaway_pubkey) = generate_key();
     let throwaway_keypair = secp256k1::KeyPair::from_secret_key(&secp, throwaway_privkey);
-    let sender_pub = get_schnorr_pub(get_privkey());
 
     let pubkey_hex = format!("03{}", recipient_pub_hex);
     let pubkey_byte_array = hex::decode(pubkey_hex).unwrap();
@@ -41,11 +40,10 @@ pub fn send_encrypted_pubkey(recipient_pub_hex: String) {
     // content is encrypted sender's pubkey
     let time = Local::now();
     let unix_time = time.timestamp();
-    let encrypted_pubkey = encrypt_ecdh(shared_priv, sender_pub.to_string());
-    // tag "#p" the recipeint pub (hex)
+    let encrypted_message = encrypt_ecdh(shared_priv, message);
     let event_id = get_event_id(
         throwaway_pubkey.to_string(),
-        encrypted_pubkey.clone(),
+        encrypted_message.clone(),
         unix_time,
         4,
         json!([["p", recipient_pub_hex]]),
@@ -53,9 +51,9 @@ pub fn send_encrypted_pubkey(recipient_pub_hex: String) {
 
     // sign id
     let event_id_byte = hex::decode(event_id.clone()).unwrap();
-    let message = Message::from_slice(&event_id_byte[..]).expect("32 bytes, within curve order");
+    let event_id_message = Message::from_slice(&event_id_byte[..]).expect("32 bytes, within curve order");
     // sign from throwaway keypair
-    let sig = secp.sign_schnorr(&message, &throwaway_keypair);
+    let sig = secp.sign_schnorr(&event_id_message, &throwaway_keypair);
 
     let event = json!({
         "id": event_id,
@@ -63,81 +61,7 @@ pub fn send_encrypted_pubkey(recipient_pub_hex: String) {
         "created_at": unix_time,
         "kind": 4,
         "tags": [["p", recipient_pub_hex]],
-        "content": encrypted_pubkey,
-        "sig": sig.to_string()
-    });
-
-    println!("{}", event);
-
-    // return event;
-}
-
-pub fn create_message(content: String, recipient_pub_hex: String) -> serde_json::Value {
-    let secp = Secp256k1::new();
-    let sender_priv = get_privkey();
-    let sender_pub = get_schnorr_pub(sender_priv);
-
-    // not precisely sure why there needs to be 0x03 or 0x02 in front
-    let pubkey_hex = format!("03{}", recipient_pub_hex);
-    let pubkey_byte_array = hex::decode(pubkey_hex).unwrap();
-    let recipient_pub =
-        PublicKey::from_slice(&pubkey_byte_array[..]).expect("32 bytes, within curve order");
-
-    let (shared_keypair, shared_priv, shared_pub) = get_shared_key(sender_priv, recipient_pub);
-
-    // todo: check whether broadcast event is present
-
-    // encrypt content
-    type Aes256Cbc = Cbc<Aes256, Pkcs7>;
-
-    // random bytes
-    let mut iv_bytes = [0u8; 16];
-    rand::thread_rng().fill(&mut iv_bytes);
-
-    let plaintext_json = json!({ sender_pub.to_string(): content });
-    let plaintext = plaintext_json.to_string();
-    let plaintext_bytes = plaintext.as_bytes();
-
-    let cipher =
-        Aes256Cbc::new_from_slices(&shared_priv.serialize_secret()[..], &iv_bytes).unwrap();
-    // buffer must have enough space for message+padding
-    // todo: figure out what the ideal buffer size is
-    let mut buffer = [0u8; 5000];
-    // copy message to the buffer
-    let pos = plaintext_bytes.len();
-    buffer[..pos].copy_from_slice(plaintext_bytes);
-    let ciphertext = cipher.encrypt(&mut buffer, pos).unwrap();
-    let ciphertext_string = format!(
-        "{}?iv={}",
-        base64::encode(ciphertext),
-        base64::encode(iv_bytes)
-    );
-
-    // create event id with encrypted data
-    let time = Local::now();
-    let unix_time = time.timestamp();
-    let event_id = get_event_id(
-        shared_pub.to_string(),
-        ciphertext_string.to_string(),
-        unix_time,
-        4,
-        json!([]),
-    );
-
-    // sign id with shared key
-    let event_id_byte = hex::decode(event_id.clone()).unwrap();
-    let message = Message::from_slice(&event_id_byte[..]).expect("32 bytes, within curve order");
-    let sig = secp.sign_schnorr(&message, &shared_keypair);
-
-    // for more information about the data below:
-    // https://github.com/fiatjaf/nostr/blob/master/nips/01.md
-    let event = json!({
-        "id": event_id,
-        "pubkey": shared_pub.to_string(),
-        "created_at": unix_time,
-        "kind": 4,
-        "tags": [],
-        "content": ciphertext_string,
+        "content": encrypted_message,
         "sig": sig.to_string()
     });
 
@@ -164,54 +88,6 @@ fn get_shared_key(
     let shared_pub = secp256k1::XOnlyPublicKey::from_keypair(&shared_keypair);
 
     return (shared_keypair, shared_privkey, shared_pub);
-}
-
-pub fn pull_and_decrypt() {
-    // pull init messages, then decrypt the content (the sender's pubkey)
-    // how do i code and test this systematically?
-
-    let sender_priv = get_privkey();
-    let sender_pub = get_schnorr_pub(sender_priv);
-    // request to relay: ["REQ", "foobar", [{"#p": sender_pub}]]
-    // todo: replace this with pulls to real relay
-
-    println!("{}", sender_pub);
-    // is this dummy event's pub the same as pub above?
-    let dummy_event = json!({
-        "content":"TNFR/PLHSeJfzbj/lVP4vjfIWAaYDhOUnLvnza6kYn8P3bgpI6Tt/A7l1fGLxicYiRW+s5r1Azw9ggbSgVB4O/P97Uuti2KeOunn5px6KWg=?iv=W4H29vgjaOsE7WZzI7Irlg==",
-        "created_at":1644088300,
-        "id":"1c141a2603af0ce19d6bd8e75e9ded03eac94df0d67c010ca8e2e37aa6e0dc86",
-        "kind":4,
-        "pubkey":"0553362b7f036acf8cb5ce0db095c7d05cee0d52293cb9818131228d726b3295",
-        "sig":"9461f1328f5e3e72fe56a92f7535f37267eff6fa719ae9abe2821b02d72c204e48c4dbc3fb2bf712a3dc9524372cfe56f60fd5bf4689da907902f95f706405b1",
-        "tags":[["p","7b27c478232bbc9791d403b1db67e9696d87e7e6fff4e57a9cdfdedb754ad475"]]
-    });
-
-    let encrypted_content_base64 = "TNFR/PLHSeJfzbj/lVP4vjfIWAaYDhOUnLvnza6kYn8P3bgpI6Tt/A7l1fGLxicYiRW+s5r1Azw9ggbSgVB4O/P97Uuti2KeOunn5px6KWg";
-    let encrypted_vec = base64::decode(encrypted_content_base64).unwrap();
-    let iv_base64 = "W4H29vgjaOsE7WZzI7Irlg==";
-    let iv_vec = base64::decode(iv_base64).unwrap();
-    let dummy_pub = "0553362b7f036acf8cb5ce0db095c7d05cee0d52293cb9818131228d726b3295";
-    let dummy_pubkey_hex = format!("03{}", dummy_pub);
-    let dummy_pubkey_byte_array = hex::decode(dummy_pubkey_hex).unwrap();
-    let dummy_recipient_pub =
-        PublicKey::from_slice(&dummy_pubkey_byte_array[..]).expect("32 bytes, within curve order");
-    let (_, shared_priv, _) = get_shared_key(sender_priv, dummy_recipient_pub);
-
-    // let mut encrypted_bytes = [0u8; 5000];
-    // // copy message to the buffer
-    // let encrypted_vec_pos = encrypted_vec.len();
-    // encrypted_bytes[..encrypted_vec_pos].copy_from_slice(&encrypted_vec[..]);
-
-    let mut iv_bytes = [0u8; 16];
-    // copy message to the buffer
-    iv_bytes.copy_from_slice(&iv_vec[..]);
-
-    println!("{:?}", iv_bytes);
-    println!("{:?}", encrypted_vec);
-    let plaintext = decrypt_ecdh(shared_priv, iv_bytes, encrypted_vec);
-
-    println!("{}", plaintext);
 }
 
 fn get_schnorr_pub(privkey: secp256k1::SecretKey) -> secp256k1::XOnlyPublicKey {
@@ -320,7 +196,6 @@ pub fn generate_config() {
         let (privkey, _) = generate_key();
         let json_data = json!({
             "privkey": privkey.display_secret().to_string(),
-            "subscription": [],
             "relay": []
         });
 
