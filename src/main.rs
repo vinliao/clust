f57b6e54065b60183176c2f01002753546be9d06
Vincent Liao <vincent.liao@protonmail.com>
2022-02-16 01:01:51 +0700

    input contact name as param (when chatting)

--- src/main.rs
+++ src/main.rs
@@ -75,18 +75,18 @@ fn main() {
 async fn run(connect_addr: &str, contact_name: &str) {
     let url = url::Url::parse(connect_addr).unwrap();
 
+    // get shared key here...
+    let recipient_pubkey_hex = util::contact_pubkey_from_name(contact_name).unwrap();
+    let shared_key = util::get_shared_key_from_hex(&recipient_pubkey_hex);
+
     let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-    tokio::spawn(read_stdin(stdin_tx));
+    tokio::spawn(read_stdin(stdin_tx, recipient_pubkey_hex));
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
 
     let (write, read) = ws_stream.split();
     let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-
-    // get shared key here...
-    let recipient_pubkey_hex = util::contact_pubkey_from_name(contact_name).unwrap();
-    let shared_key = util::get_shared_key_from_hex(&recipient_pubkey_hex);
 
     let ws_to_stdout = {
         read.for_each(|message| async {
@@ -114,13 +114,10 @@ async fn run(connect_addr: &str, contact_name: &str) {
 }
 
 // stdin stuff, modify input here
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>, recipient_pub_hex: String) {
     let mut stdin = tokio::io::stdin();
 
     // send initial payload before capturing stdin
-    let recipient_pub_hex =
-        util::contact_pubkey_from_name("branle").expect("Fail to get contact's pubkey");
-
     let shared_sha = util::get_sha_shared_key(&recipient_pub_hex);
     let initial_payload = util::to_public_dm_request(&shared_sha);
     println!("{}", initial_payload);
@@ -136,7 +133,6 @@ async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
         buf.truncate(n);
         buf.pop(); // this is a newline, basically
         let message = String::from_utf8(buf).expect("Fail turning vec to string");
-        let recipient_pub_hex = util::contact_pubkey_from_name("branle").unwrap();
         let event = util::create_public_dm_event(&recipient_pub_hex, &message);
         let payload = util::to_payload(event);
 
